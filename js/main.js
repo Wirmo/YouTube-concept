317fb59f1c611ae6e716d11ef092996520405e54
Wirmo <60112823+Wirmo@users.noreply.github.com>
2021-04-09 16:12:02 +0300

    Свёрстана основная часть сайта, добавлены слайдеры

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper('.swiper1', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 40,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next1',
+      prevEl: '.channel-button-prev1',
+    },
+  
+  });
+  var swiper = new Swiper('.swiper2', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: -100,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next2',
+      prevEl: '.channel-button-prev2',
+    },
+  
+  });
+  var swiper = new Swiper('.swiper3', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+    spaceBetween: 40,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.channel-button-next3',
+      prevEl: '.channel-button-prev3',
+    },
+  
+  });
